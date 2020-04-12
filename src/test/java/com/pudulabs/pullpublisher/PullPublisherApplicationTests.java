a07ec7548c50a6da4cfdc7835b8ccdf4f8a07846
Oliver Fierro <oliverfierro77@gmail.com>
2020-04-12 19:24:44 -0400

    initial source

---
+++ src/test/java/com/pudulabs/pullpublisher/PullPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pudulabs.pullpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
